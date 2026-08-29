bd125190c830e3f51c95fb7beddde68a0a04fe00
Krish Gupta <krigu1841@gmail.com>
2026-08-29 14:56:55 +0000

    lab01 task4

--- labs/lab01/task4/cla4.v
+++ labs/lab01/task4/cla4.v
@@ -15,6 +15,7 @@
 // so you do not need to manually chain 2-input gates.
 //   c1 = g0 + p0.cin
 //   c2 = g1 + p1.g0 + p1.p0.cin
+
 //   c3 = g2 + p2.g1 + p2.p1.g0 + p2.p1.p0.cin
 //   c4 = g3 + p3.g2 + p3.p2.g1 + p3.p2.p1.g0 + p3.p2.p1.p0.cin
 //
